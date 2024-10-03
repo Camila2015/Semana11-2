e4cf91610f84fecbe0077ed5a5597d7471e6b0dd
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-10-03 10:12:24 -0500

    Update App.py

--- App.py
+++ App.py
@@ -6,22 +6,6 @@ st.title("Tablero")
 # Barra lateral para personalizar la línea
 st.sidebar.header("Personalización de la línea")
 
-# Seleccionar tipo de línea
-line_style = st.sidebar.selectbox(
-    "Selecciona el tipo de línea",
-    ("Sólida", "Punteada", "Cortada", "Curva")
-)
-
-# Convertir la opción de línea seleccionada al estilo CSS apropiado
-if line_style == "Sólida":
-    stroke_dash = []
-elif line_style == "Punteada":
-    stroke_dash = [5, 10]
-elif line_style == "Cortada":
-    stroke_dash = [10, 10]
-elif line_style == "Curva":
-    stroke_dash = [2, 6]
-
 # Seleccionar color de la línea
 stroke_color = st.sidebar.color_picker("Selecciona el color de la línea", '#FFFFFF')
 
@@ -29,7 +13,7 @@ stroke_color = st.sidebar.color_picker("Selecciona el color de la línea", '#FFF
 stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 15)
 
 # Establecer color de fondo
-bg_color = '#000000'
+bg_color = st.sidebar.color_picker("Selecciona el color de fondo", '#000000')
 
 # Crear componente de lienzo
 canvas_result = st_canvas(
@@ -40,6 +24,5 @@ canvas_result = st_canvas(
     height=400,
     width=400,
     key="canvas",
-    drawing_mode="freedraw",
-    stroke_dash=stroke_dash  # Agregar el estilo de línea seleccionado
+    drawing_mode="freedraw"
 )
