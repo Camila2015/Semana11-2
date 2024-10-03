0a4ab48a2d974c4612f25cdc85bcf955d2ef31c0
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-10-03 10:14:37 -0500

    Update App.py

--- App.py
+++ App.py
@@ -3,19 +3,19 @@ from streamlit_drawable_canvas import st_canvas
 
 st.title("Tablero")
 
-# Barra lateral para personalizar la línea
+
 st.sidebar.header("Personalización de la línea")
 
-# Seleccionar color de la línea
+
 stroke_color = st.sidebar.color_picker("Selecciona el color de la línea", '#FFFFFF')
 
-# Seleccionar ancho de la línea
+
 stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 15)
 
-# Establecer color de fondo
+
 bg_color = st.sidebar.color_picker("Selecciona el color de fondo", '#000000')
 
-# Crear componente de lienzo
+
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Color de relleno fijo con algo de opacidad
     stroke_width=stroke_width,
